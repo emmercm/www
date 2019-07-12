8c6e4e61c5f924d4242eafb8303a684e0cafb8d1
Christian Emmer <christianemmer@stockx.com>
2019-07-12 14:17:33 -0400

    Fix View Height (#4)
    * Fix view height on resize.
    
    * Only fix view height on mobile.
    
    * Don't use the Bootstrap bundle.
    
    * Only fix on orientation change.

--- src/static/js/view-height.js
+++ src/static/js/view-height.js
@@ -5,8 +5,28 @@
  * https://stackoverflow.com/a/42965111/10317241
  */
 
-$(document).ready(function () {
-    $("[class^='vh-'], [class*=' vh-']").each(function () {
-        this.style.setProperty('height', $(this).outerHeight() + 'px', 'important');
-    })
-});
+var md = new MobileDetect(window.navigator.userAgent);
+if (md.mobile()) {
+    $(document).ready(function () {
+        $("[class^='vh-'], [class*=' vh-']").each(function () {
+            var $elem = $(this).css('transition', 'none');
+
+            var orientation = undefined;
+            var fix = function () {
+                // Replicate deprecated window.orientationchange() behavior
+                var newOrientation = $(window).height() > $(window).width() ? 'portrait' : 'landscape';
+                if (orientation !== newOrientation) {
+                    // Remove any previously set height, to re-calc height
+                    $elem.get(0).style.removeProperty('height');
+
+                    // Fix height to a specific amount
+                    $elem.get(0).style.setProperty('height', $elem.outerHeight() + 'px', 'important');
+                }
+                orientation = newOrientation;
+            };
+
+            fix();
+            $(window).resize(fix);
+        })
+    });
+}
