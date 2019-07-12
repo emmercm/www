acd8fd931d8289d87742d07f4752b97a6cbaeb55
Christian Emmer <christianemmer@stockx.com>
2019-07-11 21:46:44 -0400

    Mobile view height !important.

--- src/static/js/view-height.js
+++ src/static/js/view-height.js
@@ -1,12 +1,12 @@
 /**
  * On mobile browsers the URL bar takes away from view height, so when it hides after scrolling the view height changes.
  * For elements with vh-* Bootstrap classes, fix its height to a set number on document load.
+ *
+ * https://stackoverflow.com/a/42965111/10317241
  */
 
 $(document).ready(function () {
     $("[class^='vh-'], [class*=' vh-']").each(function () {
-        // https://stackoverflow.com/a/42965111/10317241
-        console.log(this);
-        $(this).css('height', $(this).outerHeight());
+        this.style.setProperty('height', $(this).outerHeight() + 'px', 'important');
     })
 });
