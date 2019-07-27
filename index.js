7b4f1f6639f1b32d067cadd5602d68957f116c5c
Christian Emmer <christianemmer@stockx.com>
2019-07-27 15:33:39 -0400

    target="_blank"

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const hbtmd            = require('metalsmith-hbt-md');
 const markdown         = require('metalsmith-markdown');
 const favicons         = require('metalsmith-favicons');
 const layouts          = require('metalsmith-layouts');
+const jquery           = require('metalsmith-jquery');
 const openGraph        = require('metalsmith-open-graph');
 const twitterCard      = require('metalsmith-twitter-card');
 const include          = require('metalsmith-include-files');
@@ -308,7 +309,7 @@ Metalsmith(__dirname)
         smartypants: true
     }))
 
-    // // Add favicons and icons
+    // Add favicons and icons
     .use(favicons({
         src: '**/logo3_Gray_Lighter.svg',
         appName: siteName,
@@ -331,6 +332,9 @@ Metalsmith(__dirname)
         default: 'page.hbs',
         engine: 'handlebars'
     }))
+
+    // Change all links with a protocol (external) to be target="_blank"
+    .use(jquery('**/*.html', $ => $('a[href*="://"]').attr('target', '_blank')))
 
     // Add Facebook OpenGraph meta tags
     .use(openGraph({
