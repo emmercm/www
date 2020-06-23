74a434da2245acf6716377049a06540769fbdc93
Christian Emmer <emmercm@gmail.com>
2020-06-23 14:25:28 -0400

    Use --vh CSS variable.

--- src/static/js/view-height.js
+++ src/static/js/view-height.js
@@ -1,32 +1,18 @@
 /**
  * On mobile browsers the URL bar takes away from view height, so when it hides after scrolling the view height changes.
- * For elements with vh-* Bootstrap classes, fix its height to a set number on document load.
+ * Set the --vh CSS variable on page load and window resize.
  *
- * https://stackoverflow.com/a/42965111/10317241
+ * https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
+ * http://bencentra.com/code/2015/02/27/optimizing-window-resize.html
  */
 
-var md = new MobileDetect(window.navigator.userAgent);
-if (md.mobile()) {
-    $(document).ready(function () {
-        $("[class^='vh-'], [class*=' vh-']").each(function () {
-            var $elem = $(this).css('transition', 'none');
-
-            var orientation = undefined;
-            var fix = function () {
-                // Replicate deprecated window.orientationchange() behavior
-                var newOrientation = $(window).height() > $(window).width() ? 'portrait' : 'landscape';
-                if (orientation !== newOrientation) {
-                    // Remove any previously set height, to re-calc height
-                    $elem.get(0).style.removeProperty('height');
-
-                    // Fix height to a specific amount
-                    $elem.get(0).style.setProperty('height', $elem.outerHeight() + 'px', 'important');
-                }
-                orientation = newOrientation;
-            };
-
-            fix();
-            $(window).resize(fix);
-        })
-    });
+function viewHeight() {
+    document.documentElement.style.setProperty('--vh', (window.innerHeight * 0.01) + 'px');
 }
+viewHeight();
+
+var timeout = false;
+window.addEventListener('resize', function() {
+    clearTimeout(timeout);
+    timeout = setTimeout(viewHeight, 250)
+});
