3be98c97e0a89818c9bf3677e178904ac4fce9a1
Christian Emmer <emmercm@gmail.com>
2020-07-16 14:45:23 -0700

    page-progress.js tweak.

--- src/static/js/page-progress.js
+++ src/static/js/page-progress.js
@@ -1,3 +1,7 @@
+/**
+ * Handle tags with data-page-progress attribute.
+ */
+
 $(document).ready(function () {
     $("[data-page-progress][data-page-progress!='']").each(function () {
         var $elem = $(this);
@@ -12,7 +16,7 @@ $(document).ready(function () {
 
             var zero = $elem.offset().top + parseInt($elem.css('padding-top'));
             // TODO: Not quite right, but good enough
-            var hundred = zero + $elem.height() - $(window).height();
+            var hundred = $elem.offset().top + $elem.height() - $(window).height();
 
             var progress = ($(window).scrollTop() - zero) / hundred * 100;
             if (progress < 0) {
