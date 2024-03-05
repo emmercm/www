d85b9d411ad11b7ee3709cf1db6b923c8cd1a46e
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2024-03-04 17:10:32 -0800

    process.exitCode

--- netlify-ignore-build.js
+++ netlify-ignore-build.js
@@ -1,13 +1,13 @@
 // https://docs.netlify.com/configure-builds/common-configurations/ignore-builds/
 
+process.exitCode = 0;
+
 if (process.env.BRANCH.includes('renovate')) {
     console.log('don\'t build for Renovate dependency updates');
-    process.exit(1);
+    process.exitCode = 1;
 }
 
 if (process.env.BRANCH === 'emmercm/blog-drafts') {
     console.log('don\'t build for Renovate dependency updates');
-    process.exit(1);
+    process.exitCode = 1;
 }
-
-process.exit(0);
