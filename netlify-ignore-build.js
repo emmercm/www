0eb5f1beaf32d7d1cd1af4160fa653a8ac18a006
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2024-03-04 17:12:46 -0800

    Exit code 1 means success, because of course it does

--- netlify-ignore-build.js
+++ netlify-ignore-build.js
@@ -1,13 +1,13 @@
 // https://docs.netlify.com/configure-builds/common-configurations/ignore-builds/
 
-process.exitCode = 0;
-
 if (process.env.BRANCH.includes('renovate')) {
     console.log('don\'t build for Renovate dependency updates');
-    process.exitCode = 1;
+    process.exit(0);
 }
 
 if (process.env.BRANCH === 'emmercm/blog-drafts') {
     console.log('don\'t build for Renovate dependency updates');
-    process.exitCode = 1;
+    process.exit(0);
 }
+
+process.exit(1);
