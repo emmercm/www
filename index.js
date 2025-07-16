61d914d73c227934371a3c0765a5b39dfa16ab40
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2025-07-15 20:19:22 -0700

    Fix link checker

--- index.js
+++ index.js
@@ -1095,7 +1095,7 @@ tracer(Metalsmith(path.resolve()))
             // Anti-bot 429 rate limiting
             'github.com',
             'instagram.com',
-            'linkedin.com/shareArticle',
+            'linkedin.com',
             'web.archive.org',
             // Anti-bot ECONNREFUSED
             'tldp.org',
