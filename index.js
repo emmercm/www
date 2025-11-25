f146ede784590d45a67d1b968fcb4994620871d6
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2025-11-24 21:01:20 -0800

    Blog: Defensive Shell Scripting with Shell Options

--- index.js
+++ index.js
@@ -424,6 +424,7 @@ tracer(Metalsmith(path.resolve()))
                 const imgixParameters = '&auto=format&q=80&cs=srgb&fit=crop&crop=entropy';
                 imageUrlGenerator = (width, height) => `${photo.urls.raw}${imgixParameters}&w=${width}&h=${height}`;
                 const utmParameters = '?utm_source=emmer-dev&utm_medium=referral';
+                files[filename].imageAlt = photo.alt_description;
                 files[filename].imageCredit = `Photo by <a href="${photo.user.links.html}${utmParameters}">${photo.user.name}</a> on <a href="${photo.links.html}${utmParameters}">Unsplash</a>`;
             } else {
                 imageUrlGenerator = (width, height) => `https://source.unsplash.com/${photoId}/${width}x${height}`;
