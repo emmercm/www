011d7ae7619a9097fbc8da40b22d52d59ca1d9ee
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2026-05-09 12:58:43 -0400

    Blog: Monitoring PostgreSQL Index Creation (#403)

--- index.js
+++ index.js
@@ -1186,6 +1186,8 @@ tracer(Metalsmith(path.resolve()))
             'meredithbroussard.com',
             'brunoscheufler.com',
             'zsh.org',
+            'qntm.org',
+            'nownownow.com',
         ]
     })))
 
